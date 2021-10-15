3f2ea81959e4b707c8e7402e44ce197b90f6af0d
LaGiaBach <lagiabach1610@gmail.com>
2021-10-15 15:29:47 +0700

    add category page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,30 @@
+
 $(window).ready(function() {
+    // home page js
    const popupNewletter = $('#pop-up-new-letter');
    const popupBtn = $('#pop-up-button');
    popupBtn.click(function() {
     popupNewletter.toggleClass('active');
    })
+   // category
+
+   const dropDownBtn = $('#dropdown');
+   const dropDownBlock = $('#btn-dropdown-block');
+   dropDownBtn.click(function() {
+    dropDownBlock.toggleClass('btn-dropdown-active');
+   })
+
+   //filter dropdown
+   const fillterBtnDropdown = $('#fillter-btn-dropdown');
+   const fillteBlock = $('#fillter-drop-down-block');
+   const closeBtn = $('.close');
+   fillterBtnDropdown.click(function() {
+    fillteBlock.slideToggle("slow", function() {
+
+    })
+   })
+   closeBtn.click(function() {
+    fillteBlock.slideUp("slow", function() {})
+   })
 })
+
